0685017401216fc52ada19d8490e5d780a8713d6
Gardar Thorsteinsson <gardart@gmail.com>
2017-03-16 13:02:21 +0000

    init script updated for rhel7

--- rhel7/init.sh
+++ rhel7/init.sh
@@ -1,17 +1,32 @@
-et root password with passwd and run as root
+#!/bin/sh
+
+#
+# Variables to change
+#
+
+defaultTZ=Atlantic/Reykjavik
+# RedHat Subscription Manager
+rhn_subscription_user=username@domain.com
+rhn_subscription_password=PassWord
+# Network configuration
+network_device_name=eth0
+network_connection_name=RHEL7
+defaultIP=192.168.1.100/24
+defaultGW=192.168.1.1
+defaultDNS1=192.168.1.1
+defaultDNS2=8.8.8.8
+# Puppet Configuration
+puppetmaster=puppetmaster
+
+# Do not change anything below
 
 # Set timezone
-defaultTZ=Atlantic/Reykjavik
 read -p "Enter Time Zone [$defaultTZ]: " TZ
 TZ=${TZ:-$defaultTZ}
-timedatectl set-timezone $TZ
+timedatectl set-timezone "$TZ"
 
 # Set Static IP Info - only runs once
 if [ ! -f /etc/sysconfig/network-scripts/ifcfg-rhel7 ]; then
-	defaultIP=192.168.1.100/24
-	defaultGW=192.168.1.1
-	defaultDNS1=192.168.1.1
-	defaultDNS2=8.8.8.8
 	read -p "Enter Static IP Address and CIDR [$defaultIP]: " IPADDR
 	read -p "ENTER GATEWAY [$defaultGW]: " GATEWAY
 	read -p "Enter DNS1 [$defaultDNS1]: " DNS1
@@ -20,10 +35,10 @@ if [ ! -f /etc/sysconfig/network-scripts/ifcfg-rhel7 ]; then
 	GATEWAY=${GATEWAY:-$defaultGW}
 	DNS1=${DNS1:-$defaultDNS1}
 	DNS2=${DNS2:-$defaultDNS2}
-	{ nmcli connection add type ethernet con-name CentOS7 ifname eno16777736 ip4 $IPADDR gw4 $GATEWAY &&
-	  nmcli connection modify CentOS7 ipv4.dns "$DNS1 $DNS2" &&
-	  nmcli connection up CentOS7 ifname eno16777736 &&
-	  nmcli -p connection show CentOS7 ;
+	{ nmcli connection add type ethernet con-name $network_connection_name ifname $network_device_name ip4 $IPADDR gw4 $GATEWAY &&
+	  nmcli connection modify $network_connection_name ipv4.dns "$DNS1 $DNS2" &&
+	  nmcli connection up $network_connection_name ifname $network_device_name &&
+	  nmcli -p connection show $network_connection_name ;
 	} || {
 	  echo "Failed to set IP Address" && exit ;
 	}
@@ -34,6 +49,13 @@ defaultHost=rhel7.local
 read -p "Enter Hostname [$defaultHost]: " HOSTNAME
 HOSTNAME=${HOSTNAME:-$defaultHost}
 hostnamectl set-hostname $HOSTNAME --static
+
+# Register system
+subscription-manager register --username=$rhn_subscription_user --password=$rhn_subscription_password --auto-attach
+subscription-manager repos --enable=rhel-7-server-extras-rpms
+subscription-manager repos --enable=rhel-7-server-extras-rpms
+subscription-manager repos --enable=rhel-7-server-optional-rpms
+subscription-manager repos --enable=rhel-7-server-optional-rpms
 
 # Update all packages
 yum update -y && yum -y upgrade
@@ -52,13 +74,11 @@ yum install -y nmap sed git
 ### Install Puppet Agent ###
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum -t -y -e 0 install puppet
-if $str($getVar('puppet_auto_setup','')) == "1"
 # Add puppet master to puppet.conf
-sed -i '/\[agent\]/ a\    server = puppetmaster' /etc/puppet/puppet.conf
+sed -i '/\[agent\]/ a\    server = $puppetmaster' /etc/puppet/puppet.conf
 # Restart Puppet agent
 systemctl restart puppet
-# generate puppet certificates and trigger a signing request, but
-# don't wait for signing to complete (in seconds)
-/usr/sbin/puppetd --test --waitforcert 0
+# generate puppet certificates and trigger a signing request
+puppet agent -tv
 # turn puppet service on for reboot
 systemctl enable puppet
